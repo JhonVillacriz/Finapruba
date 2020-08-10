d18133be617338acc9270c39210c75794dd24c68
Flor Cerdan <cerdanflor@gmail.com>
2020-08-10 10:09:31 -0500

    MVC - Listar Estudiante Listo

---
+++ src/java/ModeloDAO/EstudianteDAO.java
@@ -0,0 +1,64 @@
+package ModeloDAO;
+
+import Config.bd.ConectaBd;
+import Interfaces.CRUD;
+import Modelo.Estudiante;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDAO implements CRUD{
+    ConectaBd cn = new ConectaBd();
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+    Estudiante e = new Estudiante();
+
+    @Override
+    public List listarestudiante() {
+        ArrayList<Estudiante> estudiantes = new ArrayList<>();
+        String consulta = " select * "
+                        + "from estudiante ";
+        try {
+            con = cn.getConnection();
+            pst = con.prepareStatement(consulta);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                Estudiante estudiante = new Estudiante();
+                estudiante.setIdestudiante(rs.getInt("idestudiante"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellidos(rs.getString("apellidos"));
+                estudiante.setDni(rs.getString("dni"));
+                estudiante.setCodigo(rs.getString("codigo"));
+                estudiante.setEstado(rs.getString("estado"));
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e) {
+            
+        }
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarestudiante(int idestudiante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean agregarestudiante(Estudiante estudiante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean editarestudiante(Estudiante estudiante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarestudiante(int idestudiante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ web/Vista/ListarEstudiante.jsp
@@ -0,0 +1,60 @@
+<%-- 
+    Document   : ListarEstudiante
+    Created on : 10/08/2020, 09:13:41 AM
+    Author     : TuNombre
+--%>
+
+<%@page import="java.util.Iterator"%>
+<%@page import="Modelo.Estudiante"%>
+<%@page import="java.util.List"%>
+<%@page import="ModeloDAO.EstudianteDAO"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Listar Estudiantes</title>
+    </head>
+    <body>
+        <div>
+            <h1>Listado de Estudiantes</h1>
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>DNI</th>
+                        <th>Código</th>
+                        <th>Estado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <%
+                        EstudianteDAO estudianteDAO = new EstudianteDAO();
+                        List<Estudiante> estudiantes = estudianteDAO.listarestudiante();
+                        Iterator<Estudiante> iterator = estudiantes.iterator();
+                        Estudiante estudiante = null;
+                        while (iterator.hasNext()) {
+                            estudiante = iterator.next();
+
+
+                    %>
+                    <tr>
+                        <td><% out.print(estudiante.getIdestudiante()); %></td>
+                        <td><% out.print(estudiante.getNombre()); %></td>
+                        <td><% out.print(estudiante.getApellidos()); %></td>
+                        <td><% out.print(estudiante.getDni()); %></td>
+                        <td><% out.print(estudiante.getCodigo()); %></td>
+                        <td><% out.print(estudiante.getEstado()); %></td>
+                        <td></td>
+                    </tr>
+                    <%    }
+                    %>                    
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
